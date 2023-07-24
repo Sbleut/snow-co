68afe18f44f7247404535d19fb6ec0e03335198d
Sbleut <thomas.sublet@gmail.com>
2023-07-24 14:46:16 +0200

    Translation first steps

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/')]
-    public function homepage() : Response
+    public function homepage(TrickRepository $trickRepository) : Response
     {
+        $tricklist = $trickRepository->findAll();
         return $this->render('home/home.html.twig', [
             'title' => 'SnowTrick',
-            'tricks' => 'trickliste'
+            'tricks' => $tricklist
         ]);
     } 
 
