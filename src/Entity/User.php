785389b7b8aa1980512090bea63d60e4076b5460
Sbleut <thomas.sublet@gmail.com>
2023-10-02 11:57:44 +0200

    Uploading Image in Trick Create

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,6 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Constraints\PasswordStrength;
 use Symfony\Component\Uid\Uuid;
 
--- src/FileManagement/UploadImage.php
+++ src/FileManagement/UploadImage.php
@@ -3,14 +3,52 @@
 namespace App\FileManagement;
 
 use App\Entity\Image;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class UploadImage
 {
-    public function saveImage($image)
+    private $validator;
+
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function validateUploadedFile($image): void
+    {
+        // Apply Symfony validation constraints to the file
+        $violations = $this->validator->validate($image, [
+            new Assert\File([
+                'maxSize' => '5M',
+                'mimeTypes' => ['image/jpeg', 'image/png', 'image/gif'],
+            ])
+        ]);
+
+        if (count($violations) > 0) {
+            // Handle validation errors (e.g., throw an exception, log errors)
+            $errorMessages = [];
+            foreach ($violations as $violation) {
+                $errorMessages[] = $violation->getMessage();
+            }
+            throw new \Exception(implode(', ', $errorMessages));
+        }
+    }
+
+    public function saveImage($image, $trickSlug)
     {
         $file = md5(uniqid()) . '.' . $image->guessExtension();
 
-        $image->move($this->params->get('trick_image_directory'), $file);
+        $filesystem = new Filesystem();
+
+        $directory = 'uploads/image' . strtolower($trickSlug);
+
+        if (!$filesystem->exists($directory)) {
+            $filesystem->mkdir($directory);
+        }
+
+        $image->move('uploads/image' . $trickSlug . '/' , $file);
 
         return $file;
     }
